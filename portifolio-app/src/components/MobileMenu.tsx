ce2b5e19dd8abfe10bd501717dd8bd27f1db1dd0
Taurai Gombera <tauraigombera@gmail.com>
2025-04-08 23:36:30 +0200

    changed the nav links

--- portifolio-app/src/components/MobileMenu.tsx
+++ portifolio-app/src/components/MobileMenu.tsx
@@ -1,46 +1,26 @@
-import { Link, useLocation } from "react-router-dom";
-import { useState, useEffect } from "react";
-const MobileMenu = () => {
-  const navItems = ["Projects", "Blog", "Contact"];
-  const [selectedIndex, setSelectedIndex] = useState(-1);
-  const location = useLocation();
+import { Link } from "react-router-dom";
 
-  useEffect(() => {
-    const searchParams = new URLSearchParams(location.search);
-    const storedIndex = searchParams.get("selectedIndex");
-    if (storedIndex !== null) {
-      setSelectedIndex(parseInt(storedIndex));
-    }
-  }, [location.search]);
+const MobileMenu = () => {
+  const navItems = ["About","Projects", "Blog", "Contact"];
 
   return (
     <nav>
       <ul className="absolute pt-8 bg-body w-full text-4xl min-h-screen flex flex-col items-center origin-top animate-open-menu text-white md:hidden">
-        {navItems.map((navItem, index) => (
-          <li
-            className={`py-8 ${
-              selectedIndex === index ? "text-selected-text" : ""
-            }`}
-            key={navItem}
-            onClick={() => {
-              setSelectedIndex(index);
-            }}
-          >
-            <Link
-              to={
-                navItem === "Home"
-                  ? "/"
-                  : `/${navItem.toLowerCase()}?selectedIndex=${index}`
-              }
-              className="hover:text-grayText"
-            >
-              {navItem}
-            </Link>
-          </li>
-        ))}
+     
+          {navItems.map((navItem) => (
+            <li className="py-8 hover:text-gray" id="navItem" key={navItem}>
+              <Link
+                to={navItem === "Home" ? "/" : `/${navItem.toLowerCase()}`}
+              >
+                {navItem}
+              </Link>
+            </li>
+          ))}
+       
       </ul>
     </nav>
   );
 };
 
 export default MobileMenu;
+
