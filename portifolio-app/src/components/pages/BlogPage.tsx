ec3799ec6a948cea6c1915fa16fce52980e39d86
Taurai Gombera <tgombera@nfb.mw>
2026-03-05 18:56:37 +0200

    added BlogPage

---
+++ portifolio-app/src/components/pages/BlogPage.tsx
@@ -0,0 +1,13 @@
+import Header from "../Header"
+import Footer from "../Footer"
+import BlogList from "../BlogList"
+
+export default function BlogPage() {
+  return (
+    <>
+      <Header />
+      <BlogList />
+      <Footer />
+    </>
+  )
+}
