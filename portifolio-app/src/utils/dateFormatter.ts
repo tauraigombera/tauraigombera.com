455b0b29405a86663aac83e4536ca40fb50605ae
Taurai Gombera <tgombera@nfb.mw>
2026-03-06 15:35:07 +0200

    added date formatter

---
+++ portifolio-app/src/utils/dateFormatter.ts
@@ -0,0 +1,8 @@
+export function dateFormatter(dateString: string): string {
+  const date = new Date(dateString);
+  const day = date.getDate();
+  const month = date.toLocaleString("en-GB", { month: "short" });
+  const year = date.getFullYear();
+
+  return `${day} ${month}, ${year}`;
+}
