37d6ae1947de9fe4f4317b21b5380ea6a7a3b483
Taurai Gombera <tauraigombera@gmail.com>
2026-03-08 13:42:28 +0200

    updated mobile menu bg and text color

--- portifolio-app/src/components/MobileMenu.tsx
+++ portifolio-app/src/components/MobileMenu.tsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 
 const MobileMenu = () => {
-  const navItems = ["About","Projects", "Blog", "Contact"];
+  const navItems = ["About", "Posts"];
 
   return (
     <nav>
-      <ul className="absolute pt-8 bg-body w-full text-4xl min-h-screen flex flex-col items-center origin-top animate-open-menu text-white md:hidden">
+      <ul className="absolute pt-8 bg-gray w-full text-3xl min-h-screen flex flex-col items-center origin-top animate-open-menu md:hidden">
      
           {navItems.map((navItem) => (
             <li className="py-8 hover:text-gray" id="navItem" key={navItem}>
