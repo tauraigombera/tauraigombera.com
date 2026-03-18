ca5b7389f82a31b9d104b93bf4f9f76087c5ebe5
Taurai Gombera <tauraigombera@gmail.com>
2026-03-18 09:32:41 +0200

    added margin on small screen

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer>
       <Devider />
-      <div className="container flex justify-between items-center max-w-3xl mx-auto px-4 py-2 space-y-2">
+      <div className="container flex justify-between items-center max-w-3xl mx-auto px-4 py-2 space-y-2 sm: mb-8">
         <span className="text-sm text-gray-500 sm:text-center">
           &copy; {new Date().getFullYear()} Taurai Gombera
         </span>
