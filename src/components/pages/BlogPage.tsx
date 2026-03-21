82777bc027c543a11426d5773cdb67fe887a58b8
Taurai Gombera <tauraigombera@gmail.com>
2026-03-21 07:32:54 +0200

    fix text-alignment

--- src/components/pages/BlogPage.tsx
+++ src/components/pages/BlogPage.tsx
@@ -3,7 +3,7 @@ import BlogList from "../BlogList"
 export default function BlogPage() {
   return (
     <>
-      <div className="mx-auto px-4 flex flex-col items-center">
+      <div className="max-w-3xl mx-auto px-4">
         <h1 className="text-3xl font-bold mb-8">Posts</h1>
       </div>
       <div>
