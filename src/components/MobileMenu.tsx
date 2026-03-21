c5e5c3822004f0c96d9d61dbc9c546893665ee18
Taurai Gombera <tauraigombera@gmail.com>
2026-03-21 07:18:54 +0200

    fixed active selection issue

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -8,9 +8,9 @@ function MobileMenu({ onClose }: MobileMenuProps) {
   const navItems = ["Posts", "About"];
 
   return (
-    <ul className="absolute pt-8 bg-gray w-full text-3xl min-h-screen flex flex-col items-center origin-top animate-open-menu md:hidden">
+    <ul className="absolute pt-8 bg-gray w-full text-2xl font-medium min-h-screen flex flex-col items-center origin-top animate-open-menu md:hidden">
       {navItems.map((navItem) => (
-        <li className="py-4 hover:text-theme" key={navItem}>
+        <li className="py-4 hover:text-theme" id="navItem" key={navItem}>
           <NavLink
             to={navItem === "Home" ? "/" : `/${navItem.toLowerCase()}`}
             onClick={onClose}
