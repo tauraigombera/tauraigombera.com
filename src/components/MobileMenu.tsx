4af4ca590e2635832e2e64667502524e4e6d7463
Taurai Gombera <tauraigombera@gmail.com>
2026-03-22 21:21:40 +0200

    added darkmode

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -8,7 +8,7 @@ function MobileMenu({ onClose }: MobileMenuProps) {
   const navItems = ["Posts", "About"];
 
   return (
-    <ul className="absolute pt-8 bg-gray w-full text-2xl font-medium min-h-screen flex flex-col items-center origin-top animate-open-menu md:hidden">
+    <ul className="absolute pt-8 bg-gray dark:bg-darkTheme dark:text-gray w-full text-2xl font-medium min-h-screen flex flex-col items-center origin-top animate-open-menu md:hidden">
       {navItems.map((navItem) => (
         <li className="py-4 hover:text-theme" id="navItem" key={navItem}>
           <NavLink
